b665838dd066e6142164854505fbec4345f5e6b2
katsura-jp <nok.addayo@gmail.com>
2023-03-05 16:02:54 +0900

    Change: toggleIME cmd -> ctrl+space

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,20 +1,9 @@
 -- key bind
-single_key_flag = true
-single_key_timer = hs.timer.delayed.new(0.2, function()
-    single_key_flag = false
-end)
-
--- toggle on -> start true, stop true
--- toggle off -> start true, stop false
--- issue: start falseの場合がある
 local function toggleIME(event)
-	local c = event:getKeyCode()
-	local f = event:getFlags()
-
-	if not f['cmd'] and c == hs.keycodes.map['cmd'] then
-        single_key_timer:stop()
-        print("stop", single_key_flag)
-        if single_key_flag then
+    local c = event:getKeyCode()
+    local f = event:getFlags()
+    if c == hs.keycodes.map['space'] then
+        if f.ctrl then
             if hs.keycodes.currentMethod() == 'Romaji' then
                 hs.keycodes.setMethod('Hiragana')
                 hs.alert.show("かな", hs.styledtext, hs.screen.mainScreen(), 0.2)
@@ -22,13 +11,7 @@ local function toggleIME(event)
                 hs.keycodes.setMethod('Romaji')
                 hs.alert.show("ABC", hs.styledtext, hs.screen.mainScreen(), 0.2)
             end
-        else
-            single_key_flag = true
         end
-    else
-        single_key_flag = true
-        single_key_timer:start()
-        print("start", single_key_flag)
     end
 end
 
@@ -42,7 +25,7 @@ local function esc2eng(event)
     end
 end
 
-eikana = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, toggleIME)
+eikana = hs.eventtap.new({hs.eventtap.event.types.keyUp}, toggleIME)
 eikana:start()
 
 escape2english = hs.eventtap.new({hs.eventtap.event.types.keyUp}, esc2eng)
