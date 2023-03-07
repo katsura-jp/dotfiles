50a6f471a1e398afea10f1530a90bf97d6dca80c
Naoki Katsura <naoki.katsura@sportip.jp>
2023-03-07 17:54:41 +0900

    Update nvim keymap

--- config/nvim/lua/keymap.lua
+++ config/nvim/lua/keymap.lua
@@ -1,2 +1,11 @@
+local opts = { noremap = true, silent = true }
+
 vim.keymap.set("n", "<S-Right>", "<Nop>")
 vim.keymap.set("n", "<S-Left>", "<Nop>")
+vim.keymap.set("n", "q:", "<Nop>")
+
+vim.keymap.set("i", "<C-a>", "<HOME>", opts)
+vim.keymap.set("i", "<C-e>", "<END>", opts)
+vim.keymap.set("n", "<C-a>", "<HOME>", opts)
+vim.keymap.set("n", "<C-e>", "<END>", opts)
+
