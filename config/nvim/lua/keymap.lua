33d7cbeb72af93ea022e4c1e8663a714aead63c0
Naoki Katsura <naoki.katsura@sportip.jp>
2023-03-07 18:31:57 +0900

    Remove default keymap

--- config/nvim/lua/keymap.lua
+++ config/nvim/lua/keymap.lua
@@ -3,6 +3,7 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<S-Right>", "<Nop>")
 vim.keymap.set("n", "<S-Left>", "<Nop>")
 vim.keymap.set("n", "q:", "<Nop>")
+vim.keymap.set("i", "<C-Space>", "<Nop>")
 
 vim.keymap.set("i", "<C-a>", "<HOME>", opts)
 vim.keymap.set("i", "<C-e>", "<END>", opts)
