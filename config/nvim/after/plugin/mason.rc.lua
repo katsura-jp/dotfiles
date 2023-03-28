8b6b689d515ac952713e27e1ffbf901920fe0807
Naoki Katsura <nok.addayo@gmail.com>
2023-03-28 16:43:57 +0900

    Update mason version

--- config/nvim/after/plugin/mason.rc.lua
+++ config/nvim/after/plugin/mason.rc.lua
@@ -11,51 +11,6 @@ mason.setup({
   }
 })
 
--- keymap
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', 'H', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, bufopts)
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, bufopts)
-  vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist, bufopts)
-  vim.keymap.set('n', '<Leader>lf', "<cmd>lua vim.lsp.buf.format({async=true})<CR>", bufopts)
-  vim.keymap.set('n', 'cc', vim.lsp.buf.incoming_calls, bufopts)
-  -- Reference highlight
-  vim.cmd [[
-    highlight LspReferenceText  cterm=underline ctermbg=8 gui=underline guibg=#104040
-    highlight LspReferenceRead  cterm=underline ctermbg=8 gui=underline guibg=#104040
-    highlight LspReferenceWrite cterm=underline ctermbg=8 gui=underline guibg=#104040
-    set updatetime=100
-    augroup lsp_document_highlight
-      autocmd! * <buffer>
-      autocmd CursorHold,CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
-      autocmd CursorMoved,CursorMovedI <buffer> lua vim.lsp.buf.clear_references()
-    augroup END
-  ]]
-end
-
-
 local servers = { 'pyright', 'lua_ls', 'bashls' }
 
 local status, mason_lspconfig = pcall(require, 'mason-lspconfig')
@@ -66,13 +21,7 @@ mason_lspconfig.setup({ ensure_installed = servers })
 local status, lspconfig = pcall(require, 'lspconfig')
 if not status then return end
 
-for _, lsp in ipairs(servers) do lspconfig[lsp].setup({ on_attach = on_attach }) end
-
--- LSP handlers
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true }
-)
-
+for _, lsp in ipairs(servers) do lspconfig[lsp].setup({}) end
 
 lspconfig.lua_ls.setup {
   settings = {
@@ -82,5 +31,56 @@ lspconfig.lua_ls.setup {
       }
     }
   },
-  on_attach = on_attach
 }
+
+
+-- keymap
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    local bufopts = { noremap = true, silent = true, buffer = ev.buf }
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', 'H', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, bufopts)
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, bufopts)
+    vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist, bufopts)
+    vim.keymap.set('n', '<Leader>lf', "<cmd>lua vim.lsp.buf.format({async=true})<CR>", bufopts)
+    vim.keymap.set('n', 'cc', vim.lsp.buf.incoming_calls, bufopts)
+    -- Reference highlight
+    vim.cmd [[
+      highlight LspReferenceText  cterm=underline ctermbg=8 gui=underline guibg=#104040
+      highlight LspReferenceRead  cterm=underline ctermbg=8 gui=underline guibg=#104040
+      highlight LspReferenceWrite cterm=underline ctermbg=8 gui=underline guibg=#104040
+      set updatetime=100
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold,CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved,CursorMovedI <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]]
+  end
+})
+
+-- LSP handlers
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true }
+)
+
+
