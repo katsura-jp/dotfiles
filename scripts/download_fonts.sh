e0d10a07f42a1771cde39e91f3d265f48183de1d
katsura-jp <nok.addayo@gmail.com>
2025-12-17 21:10:20 +0900

    Add functionality to download and extract additional fonts from a zip file in the download_fonts.sh script

--- scripts/download_fonts.sh
+++ scripts/download_fonts.sh
@@ -20,3 +20,11 @@ for font in ${fonts[@]}; do
 done
 
 wget https://github.com/adobe-fonts/source-han-code-jp/releases/download/2.012R/SourceHanCodeJP.ttc -P $font_dir
+
+# Download and extract fonts from zip
+zip_url="https://github.com/yuru7/moralerspace/releases/download/v2.0.0/Moralerspace_v2.0.0.zip"
+tmp_dir=$(mktemp -d)
+wget "$zip_url" -O "$tmp_dir/font.zip"
+unzip "$tmp_dir/font.zip" -d "$tmp_dir"
+find "$tmp_dir" -name "*.ttf" -exec mv {} "$font_dir" \;
+rm -rf "$tmp_dir"
